ccb79a5a30c0491d32a0c53f834c7fdfa72bae05
Danilo Oliveira <danilo@sigalei.com.br>
2023-09-16 08:59:37 -0300

    feat: integrate routes in the application

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { ReactNode }  from 'react';
 import './App.css';
-import Login from './screens/Login';
 
-function App() {
+function App(props:{children:ReactNode}) {
+
   return (
     <div className="App">
-      <Login />
+      {props.children}
     </div>
   );
 }
