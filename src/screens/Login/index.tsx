0a162e6a2829c6790f8bda090152579006879846
Danilo Oliveira <danilo@sigalei.com.br>
2023-09-15 22:51:40 -0300

    feat: add login fetch

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { useState, useEffect } from 'react';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Box from '@mui/material/Box';
@@ -6,10 +7,65 @@ import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import CardActions from '@mui/material/CardActions';
+import { FormControl, InputLabel, 
+        FilledInput, InputAdornment, IconButton, Typography } from '@mui/material';
+import { VisibilityOff, Visibility } from '@mui/icons-material'
 
 import {CustomizedCardHeader} from './styles';
+
 const Login = () => {
-    return (
+  
+  const [isButtonActive, setIsButtonActive] = useState(true);
+  const [username, setUsername] = useState<string|null>(null)
+  const [password, setPassword] = useState<string|null>(null)
+  const [showPassword, setShowPassword] = useState(false);
+  const [errorMessage, setErrorMessage] = useState<string|null>(null)
+
+  const handleClickShowPassword = () => {
+    setShowPassword(!showPassword);
+  }
+  
+  useEffect(() => {
+    if(username !== null && username !== '' &&
+       password !== null && password !== '') {
+        setIsButtonActive(false);
+      } else {
+        setIsButtonActive(true);
+      }
+  }, [username, password]);
+
+  const postLogin = () => {
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ 
+        login: username,
+        senha: password
+      })
+  };
+    setErrorMessage('');
+    fetch('http://localhost:3000/usuarios/login', requestOptions)
+      .then(async (response) => {
+        const dataResponse = await response.json()
+        return {
+          responseStatus: response.status,
+          data: dataResponse
+        }
+      })
+      .then(data => {
+        console.log('sucesso', JSON.stringify(data))
+        console.log(data)
+        if(data.responseStatus === 422 && data.data?.mensagem) {
+          setErrorMessage(data.data?.mensagem)
+        } else if(data.responseStatus === 400) {
+          setErrorMessage('Requisição inválida!')
+        } else if(data.responseStatus === 200) {
+          alert('Requisição válida!')
+        } 
+      })
+      .catch(error => setErrorMessage('Erro no servidor, tente novamente em alguns minutos!'));
+  }
+  return (
       <Box sx={{
         width: '100%',
         height: '100%',
@@ -25,18 +81,66 @@ const Login = () => {
           />
           <CardContent>
             <Box py={1}>
-              <TextField fullWidth id="username" label="Usuário" variant="filled" />
+              <TextField
+                onChange={(newValue) => {
+                  setUsername(newValue.target.value);
+                }}
+                fullWidth id="username"
+                label="Usuário"
+                variant="filled"
+              />
             </Box>
             <Box py={1}>
-              <TextField fullWidth id="password" label="Senha" variant="filled" />
+            <FormControl sx={{ width: '100%' }} variant="filled">
+              <InputLabel htmlFor="filled-adornment-password">Password</InputLabel>
+              <FilledInput
+                onChange={(newValue) => {
+                  setPassword(newValue.target.value);
+                }}
+                id="filled-adornment-password"
+                type={showPassword ? 'text' : 'password'}
+                endAdornment={
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      onClick={handleClickShowPassword}
+                      edge="end"
+                    >
+                      {showPassword ? <VisibilityOff /> : <Visibility />}
+                    </IconButton>
+                  </InputAdornment>
+                }
+              />
+            </FormControl>
             </Box>
           </CardContent>
           <CardActions>
-            <Button fullWidth variant="contained">Login</Button>
+            <Box sx={{
+              display:'flex',
+              flexDirection:'row',
+              width:'100%',
+              flexWrap: 'wrap'
+            }}>
+
+              <Box width={'100%'}>
+                {errorMessage && <Typography color={'red'}>
+                  {errorMessage}
+                </Typography>}
+              </Box>
+              <Box width={'100%'}>
+                
+              <Button
+                sx={{
+                  width: '100%'
+                }}
+                onClick={() => {postLogin()}}
+                disabled={isButtonActive} fullWidth variant="contained">Login</Button>
+              </Box>
+            </Box>
           </CardActions>
         </Card>
       </Box>
-    )
+  )
 }
 
 
