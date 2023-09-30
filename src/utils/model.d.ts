f139d1f452e52242584b1e9e3b38c9600beb2afe
Danilo Oliveira <danilo@sigalei.com.br>
2023-09-30 09:45:35 -0300

    feat: update types

--- src/utils/model.d.ts
+++ src/utils/model.d.ts
@@ -1,4 +1,16 @@
 export type Categoria = {
-    id: number,
-    descricao: string
-}
+  id: number;
+  descricao: string;
+};
+
+
+
+type IdTarefa = number;
+
+export type Tarefa = {
+  descricao: string;
+  id_categoria: number;
+  id: IdTarefa;
+  id_usuario: number;
+  data_conclusao: Date | null;
+};
