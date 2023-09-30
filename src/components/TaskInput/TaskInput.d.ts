6acf574cc735559c6e38a6bed80c88bc13e55d0f
Danilo Oliveira <danilo@sigalei.com.br>
2023-09-30 11:24:10 -0300

    refactor: taskinput

--- src/components/TaskInput/TaskInput.d.ts
+++ src/components/TaskInput/TaskInput.d.ts
@@ -1,6 +1,10 @@
-import { Categoria } from '../../utils/model';
+import { Categoria, Tarefa } from '../../utils/model';
+
+
 
 export type TaskInputProps = {
-    onSelectCreateTask: (category:string|null) => void;
     category: Categoria;
+    task?: Tarefa;
+    cancelTask: () => void;
+    submitTask: () => void;
 }
